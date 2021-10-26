27b009020cbc33483cc8416d9a80cad33332707f
CyJaySong <cyjaysong@cqgynet.com>
2021-10-26 08:52:20 +0800

    fix Android V2 embedding

--- example/android/app/src/main/kotlin/com/baidu/bdmap_location_flutter_plugin_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/baidu/bdmap_location_flutter_plugin_example/MainActivity.kt
@@ -1,12 +1,6 @@
 package com.baidu.bdmap_location_flutter_plugin_example
 
-import android.os.Bundle
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
+class MainActivity : FlutterActivity() {
 }
